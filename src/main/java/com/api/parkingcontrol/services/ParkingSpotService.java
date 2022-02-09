15f93729745fc2c5639c571b68d5f7d97d7452ef
kayque <kayquegps2@gmail.com>
2022-02-08 21:41:31 -0300

    create spring boot project and add post method

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,20 @@
+package com.api.parkingcontrol.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+
+	@Autowired
+	ParkingSpotRepository parkingSpotRepository;
+
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+}
