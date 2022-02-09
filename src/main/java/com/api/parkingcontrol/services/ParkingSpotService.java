1a7eb1661a0e463fdd80046bcce8ebcab50a1950
kayque <kayquegps2@gmail.com>
2022-02-09 19:57:37 -0300

    add dateConfig and pageable

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,9 +36,9 @@ public class ParkingSpotService {
 		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
 	}
 
-	public List<ParkingSpotModel> findAll() {
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
 
-		return parkingSpotRepository.findAll();
+		return parkingSpotRepository.findAll(pageable);
 	}
 
 	public Optional<ParkingSpotModel> findById(UUID id) {
